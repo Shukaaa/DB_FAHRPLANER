6b62bc33b8c2db21507873bf3e72735c99ed4a9c
Shukaaa <asappokimane@gmail.com>
2023-01-18 08:35:56 +0100

    title

--- app/index.php
+++ app/index.php
@@ -28,7 +28,7 @@ if (isset($_POST["bahnhof_start"]) && isset($_POST["bahnhof_ziel"])) {
     <link rel="stylesheet" href="./css/main.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
 
-    <title>DB Fahrplan</title>
+    <title>Fluffiger Fahrplaner</title>
 </head>
 <body>
     <main class="center-hv">
